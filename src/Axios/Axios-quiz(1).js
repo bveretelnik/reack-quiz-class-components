44dda99e09ba5524eab7c4fd89d11353cdaf8eef
Bogdan <bveretelnik@gmail.com>
2020-11-24 11:37:25 +0200

    Initialization

--- src/Axios/Axios-quiz(1).js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-export default axios.create({
-    baseURL: 'https://react-quiz-e0f35.firebaseio.com/'
-})
-
-
--- src/hoc/Auxilliry/Auxilliry(1).js
+++
@@ -1,3 +0,0 @@
-const Auxilliry = props => props.children
-
-export default Auxilliry
